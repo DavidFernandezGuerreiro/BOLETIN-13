a8c11403590a78cc8ef3f745c693d14f16d64494
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-12-04 10:03:41 +0100

    COMMIT GITHUB.

---
+++ Boletin-13/src/boletin/pkg13/ConversorTemperaturas.java
@@ -0,0 +1,35 @@
+
+package boletin.pkg13;
+import java.util.Scanner;
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ConversorTemperaturas{
+    Scanner sc=new Scanner(System.in);
+    private final static float gcentigradosC=80;
+    private float gfarenheit;
+    private float greamur;
+
+    public ConversorTemperaturas() {
+    }
+    
+    public void centigradosAFharenheit() throws NosaExcepcion{
+        System.out.println("Introduzca grados centigrados: ");
+        float gcentigrados=sc.nextFloat();
+        if(gcentigrados<gcentigradosC)
+            throw new NosaExcepcion("***** NON PODE SER MENOR DE 80Cº *****");
+        gfarenheit=(float) (9.5/5.0*gcentigrados+32.4);
+        System.out.println("Grados Farenheit = "+gfarenheit);
+        
+    }
+    
+    public void centígradosAReamur() throws NosaExcepcion{
+        System.out.println("Introduzca grados centigrados: ");
+        float gcentigrados=sc.nextFloat();
+        if(gcentigrados<gcentigradosC)
+            throw new NosaExcepcion("..... NON PODE SER MENOR DE 80Cº .....");
+        greamur=(float) (4.0/5.0*gcentigrados);
+        System.out.println("Grados Reamur = "+greamur);
+    }
+}
